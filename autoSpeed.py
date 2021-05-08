a3c9ab1010674a060b8babd5b8cf587a7f5afe15
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-05-08 19:38:03 +0530

    fix time

--- autoSpeed.py
+++ autoSpeed.py
@@ -5,8 +5,8 @@ from selenium import webdriver # the holy webdriver to control all all the comma
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 # print("\n\nHello, welcome to the automated speed scanner:\n\n")
 
@@ -14,29 +14,36 @@ from selenium.webdriver.support import expected_conditions as EC
 PATH = "D:\Installed_Programs\chromedriver.exe"
 driver = webdriver.Chrome(PATH) #storing the webdriver in a variable
 
-driver.get("https://fast.com/") #this time its an custom made website to test web threats.
+url = "https://fast.com/"
+driver.get(url) #this time its an custom made website to test web threats.
 print("\n\n")
 
-infoLink = driver.find_element_by_id("show-more-details-link")
-time.sleep(10)
+wait = WebDriverWait(driver, 30)
+
+infoLink = wait.until(
+        EC.element_to_be_clickable(
+        (By.XPATH, '/html/body/div/div[2]/div[1]/div[4]/div[1]/a')) 
+        )
 
 status = infoLink.is_displayed()
 
-print(status)
+#CHECK THE STATUS FOR PEACE OF MIND
+# print(status)
 
 
 if(status ==  True):
     #the speed values be it in 100 or thousands
-    speedvalue = WebDriverWait(driver,10).until(
+    speedvalue = wait.until(
         EC.presence_of_element_located(
         (By.ID, 'speed-value')) 
         )
-    # print (speedvalue.text)
+
     #for the speed test units be it in mpbs or kbps acc. to the browser
-    speedunits = WebDriverWait(driver,10).until(
+    speedunits = wait.until(
         EC.presence_of_element_located(
         (By.ID, 'speed-units')) 
         )
+    print("Your connection speed is :")
     print (speedvalue.text ,speedunits.text)
 
 driver.quit()
